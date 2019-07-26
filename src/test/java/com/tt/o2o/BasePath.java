6476c2cfb14106ef82beb4188c8c7ced0bd9ddbb
Taotao <tt416926063@qq.com>
2019-07-26 14:25:47 +0800

    解决Error creating bean with name 'org.springframework.web.servlet.resource.ResourceHttpRequestHandler#0'

--- src/test/java/com/tt/o2o/BasePath.java
+++ src/test/java/com/tt/o2o/BasePath.java
@@ -3,8 +3,10 @@ package com.tt.o2o;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/spring-*.xml"})
+@WebAppConfiguration("src/main/resources")
 public class BasePath {
 }
